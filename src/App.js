9b1dc1e5b15d0d25ec06f2d47cfe62e525622e29
Alexander <akrimec@gmail.com>
2023-07-27 15:03:19 +0300

    added photo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import Partners from "./components/partners/partners";
 import "./app.css"
 import AppBanner from "./components/banner/banner";
 import AppMenu from "./components/menu/menu";
+import Companies from "./components/companies/companies";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <AppMenu/>
         <AppBanner/>
-        <Partners/>
+          <div className="App-body">
+              <Partners/>
+              <Companies/>
+          </div>
       </header>
     </div>
   );
