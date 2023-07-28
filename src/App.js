95f84f1de3e1e0cb005d86cf12b7e9562296554c
Alexander <akrimec@gmail.com>
2023-07-28 17:42:05 +0300

    create black background

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import "./app.css"
 import AppBanner from "./components/banner/banner";
 import AppMenu from "./components/menu/menu";
 import Companies from "./components/companies/companies";
+import About from "./components/about/about";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <AppMenu/>
-        <AppBanner/>
-        <div className="App-body">
-            <Partners/>
-            <Companies/>
+    <div className="back">
+        <div className="App">
+          <header className="App-header">
+            <AppMenu/>
+            <AppBanner/>
+            <div className="App-body">
+                <Partners/>
+                <Companies/>
+                <About/>
+            </div>
+          </header>
         </div>
-      </header>
     </div>
   );
 }
