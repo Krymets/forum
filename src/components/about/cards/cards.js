9ebe24c00193be798db27c8c292e6284f317907f
Alexander <akrimec@gmail.com>
2023-07-28 17:42:42 +0300

    create ellipses, cards components

---
+++ src/components/about/cards/cards.js
@@ -0,0 +1,53 @@
+import './cards.css';
+
+const AppCards = () => {
+    return (
+      <div className="Frame47-a">
+        <div className="about-text-why">ЧОМУ ВАРТО</div>
+        <div className="Frame55-a">
+          <div className="Card1">
+            <div className="Frame50-a">
+              <div className="Frame54-a">
+                <img className="Intersect" src="/svg/intersect.svg" alt=""/>
+                <img className="Expert" src="/svg/expert.svg" alt=""/>
+              </div>
+              <div className="Card1-text-header">ЕКСПЕРТНІСТЬ</div>
+              <div className="Card1-text">Спілкування з провідними експертами і практиками ексклюзивна.</div>
+            </div>
+          </div>
+          <div className="Card2">
+            <div className="Frame50-a">
+              <div className="Frame54-a">
+                <img className="Intersect" src="/svg/intersect.svg" alt=""/>
+                <img className="Expert" src="/svg/contacts.svg" alt=""/>
+              </div>
+              <div className="Card1-text-header">НОВІ КОНТАКТИ</div>
+              <div className="Card1-text">Нові ділові контакти та переговори презентація Вашого бренду, компанії, рішень і послуг.</div>
+            </div>
+          </div>
+          <div className="Card2">
+            <div className="Frame50-a">
+              <div className="Frame54-a">
+                <img className="Intersect" src="/svg/intersect.svg" alt=""/>
+                <img className="Expert" src="/svg/info.svg" alt=""/>
+              </div>
+              <div className="Card1-text-header">ПРАКТИЧНІСТЬ</div>
+              <div className="Card1-text">Практична інформація від представників галузей.</div>
+            </div>
+          </div>
+          <div className="Card2">
+            <div className="Frame50-a">
+              <div className="Frame54-a">
+                <img className="Intersect" src="/svg/intersect.svg" alt=""/>
+                <img className="Expert" src="/svg/exchange.svg" alt=""/>
+              </div>
+              <div className="Card1-text-header">ОБМІН</div>
+              <div className="Card1-text">Обмін практичними напрацюваннями та досвідом.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default AppCards;
---
+++ src/components/menu/ellipses/ellipses.js
@@ -0,0 +1,30 @@
+import './ellipses.css';
+
+export const GreenEllipses = () => {
+  return (
+    <div className="Ellipses">
+      <div className="Ellipse-green"/>
+      <div className="Ellipse-green"/>
+      <div className="Ellipse-green"/>
+      <div className="Ellipse-green"/>
+      <div className="Ellipse-green"/>
+      <div className="Ellipse-green"/>
+      <div className="Ellipse-green"/>
+    </div>
+  );
+};
+
+
+export const WhiteEllipses = () => {
+  return (
+    <div className="Ellipses">
+      <div className="Ellipse-white"/>
+      <div className="Ellipse-white"/>
+      <div className="Ellipse-white"/>
+      <div className="Ellipse-white"/>
+      <div className="Ellipse-white"/>
+      <div className="Ellipse-white"/>
+      <div className="Ellipse-white"/>
+    </div>
+  );
+}
